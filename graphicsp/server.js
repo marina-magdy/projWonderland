b70e1d524f5daee01d1cebffbd7a7a5418daea67
marina-magdy <49943493+marina-magdy@users.noreply.github.com>
2021-01-22 12:59:14 +0200

    Update server.js

--- graphicsp/server.js
+++ graphicsp/server.js
@@ -5,6 +5,6 @@ var http= require('http').Server(app);
 var io= require('socket.io')(http);
 var port= process.env.PORT || 5000;
 app.use(express.static('public'));
-var listener = app.listen(8888, function(){
-    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+http.listen(port,function(){
+    console.log('listening on *:'+port);  
 });
